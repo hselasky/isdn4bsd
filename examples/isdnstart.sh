1da31199044f3b7ab54ccfe37913115cc6b97e18
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2005-10-24 19:33:06 +0000

    Added ISDN start example script.

---
+++ examples/isdnstart.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#
+# This is an example startup shell 
+# for ISDN4BSD on NetBSD.
+# Don't forget to "chmod +x isdnstart.sh"
+#
+
+[ `uname` != "NetBSD" ] && echo "Operating system mismatch!" && exit
+
+#
+# only execute once
+#
+modstat -n i4b > /dev/null && echo "ISDN4BSD already loaded (skipping init)" && exit
+
+echo "Starting ISDN4BSD ..."
+modload -o i4b.t /usr/lkm/i4b.o
+
+rm -f /dev/i4b*
+rm -f /dev/ihfc*
+rm -f /dev/capi20
+
+#
+# TODO: mirror /dev/dev/* in /dev/
+#
+
+ln -s /dev/dev/i4b /dev/i4b
+ln -s /dev/dev/i4bctl /dev/i4bctl
+ln -s /dev/dev/capi20 /dev/capi20
+ln -s /dev/dev/i4btrc0 /dev/i4btrc0
+ln -s /dev/dev/i4btrc1 /dev/i4btrc1
+ln -s /dev/dev/i4btrc7 /dev/i4btrc7
+
+rm -f -r "/dev/dev"
+mkdir "/dev/dev"
+mount_devfs "/dev/dev"
+
+#
+# set NT-mode for controller 1
+#
+
+#
+# isdnconfig -u 1 -p DRVR_DSS1_NT
+#
