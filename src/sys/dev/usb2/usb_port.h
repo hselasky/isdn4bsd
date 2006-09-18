7c15a7fcdd3fba072ec286df5f1ac2a9fc6a6bd3
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2006-09-18 17:30:30 +0000

    usb: Remove duplicate macro.

--- src/sys/dev/usb2/usb_port.h
+++ src/sys/dev/usb2/usb_port.h
@@ -310,7 +310,6 @@ typedef struct thread *usb_proc_ptr;
 #define USBBASEDEVICE        device_t
 #define USBDEV(bdev)         (bdev)
 #define USBDEVNAME(bdev)     device_get_nameunit(bdev)
-#define USBDEVUNIT(bdev)     device_get_unit(bdev)
 #define USBDEVPTRNAME(bdev)  device_get_nameunit(bdev)
 #define USBDEVUNIT(bdev)     device_get_unit(bdev)
 #define USBGETSOFTC(bdev)    device_get_softc(bdev)
