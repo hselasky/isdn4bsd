f794d82266fac24ea129bd4f2e0330f1feee8f7a
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2007-05-06 12:20:10 +0000

    I4B+USB: Don't install if_ural.c by default, hence it breaks compilation on FreeBSD-6.2 RELEASE.

--- FreeBSD.usb/install_uaudio.sh
+++ FreeBSD.usb/install_uaudio.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Installing uaudio ..."
 cp -i ../src/sys/dev/sound/usb/uaudio*[ch] /sys/dev/sound/usb/
 cp -i ../src/sys/dev/sound/pcm/mixer*[ch] /sys/dev/sound/pcm/
+echo "Done."
---
+++ FreeBSD.usb/install_ural.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Installing if_ural ..."
+cp -i ../src/sys/dev/usb/if_ural*[ch] /sys/dev/usb/
+echo "Done."
